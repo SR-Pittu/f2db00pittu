e0bc1c6ac5f7da742c30e918d5b85ba855e0074a
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2022-11-09 19:39:08 -0600

    screen shot 5 done

--- controllers/university.js
+++ controllers/university.js
@@ -1,21 +1,21 @@
 var university = require('../models/university');
 // List of all Costumes
-exports.costume_list = function(req, res) {
+exports.university_list = function(req, res) {
 res.send('NOT IMPLEMENTED: university list');
 };
 // for a specific Costume.
-exports.costume_detail = function(req, res) {
+exports.university_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: university detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.costume_create_post = function(req, res) {
+exports.university_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: university create POST');
 };
 // Handle Costume delete form on DELETE.
-exports.costume_delete = function(req, res) {
+exports.university_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: university delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.costume_update_put = function(req, res) {
+exports.university_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: university update PUT' + req.params.id);
 };
