bde1ea6b3b1997cf6e71a8018db9104901331121
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2022-11-15 19:29:44 -0600

    added detail page

--- routes/university.js
+++ routes/university.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', university.university_view_all_Page);
+router.get('/detail', university.university_view_one_Page);
 
 module.exports = router;
