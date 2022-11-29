63e92f41be2f2ac59439476d15dbb645d4fc2692
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2022-11-29 00:31:10 -0600

    added buttons

--- models/university.js
+++ models/university.js
@@ -7,8 +7,8 @@ const unversitySchema = mongoose.Schema({
     University_Location: String,
     Number_of_Courses_Offered: {
         type: Number,
-        min: [10, "Min value is 10"],
-        max: [400, "Max value is 400"]
+        min: [50, "Min value is 50"],
+        max: [600, "Max value is 600"]
     }
 })
 module.exports = mongoose.model("university",
