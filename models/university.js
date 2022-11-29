d32b00392953e7552626fece72a911ea5098bec9
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2022-11-28 22:57:37 -0600

    added validator

--- models/university.js
+++ models/university.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose")
 const unversitySchema = mongoose.Schema({
-    University_Name: String,
+    University_Name:  {
+        type: String,
+        required: [true, "Name required"]
+    },
     University_Location: String,
-    Number_of_Courses_Offered: Number
+    Number_of_Courses_Offered: {
+        type: Number,
+        min: [10, "Min value is 10"],
+        max: [400, "Max value is 400"]
+    }
 })
 module.exports = mongoose.model("university",
     unversitySchema)
--- routes/university.js
+++ routes/university.js
@@ -2,11 +2,21 @@ var express = require('express');
 const university = require('../controllers/university');
 var router = express.Router();
 
+
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', university.university_view_all_Page);
 router.get('/detail', university.university_view_one_Page);
-router.get('/create', university.university_create_Page);
-router.get('/update', university.university_update_Page);
-router.get('/delete', university.university_delete_Page);
+router.get('/create', secured, university.university_create_Page);
+router.get('/update', secured, university.university_update_Page);
+router.get('/delete', secured, university.university_delete_Page);
+
 
 module.exports = router;
